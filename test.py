fb209cf75ac9251b6d3693ddc927a108cb0695e0
Shanky2128 <tomarshanu2128@gmail.com>
2026-03-05 17:06:27 +0530

    Remove extra comments

--- test.py
+++ test.py
@@ -1,26 +1,4 @@
-# import requests
-# import os
-# from dotenv import load_dotenv
 
-# # Load variables from .env
-# load_dotenv()
-
-# # Get API key
-# api_key = os.getenv("RAPID_API_KEY")
-
-# url = "https://article-extractor2.p.rapidapi.com/article/parse"
-
-# querystring = {"url":"https://medium.com/sharing-food/i-ate-my-way-through-singapore-and-absolutely-loved-it-part-1-36c2f0296a55","region":"eu"}
-
-
-# headers = {
-# 	"x-rapidapi-key": api_key,
-# 	"x-rapidapi-host": "article-extractor2.p.rapidapi.com"
-# }
-
-# response = requests.get(url, headers=headers, params=querystring)
-
-# print(response.json())
 
 import requests
 import os
